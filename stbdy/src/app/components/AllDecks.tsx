5e9ce3fe1fcc856c8aadd8e6356ed1daa182d1fb
kerokero014 <kevinjrm.kr@gmail.com>
2024-06-04 19:39:41 -0600

    refactor: Update AllDecks and Deck components layout and remove unnecessary transition effect

--- stbdy/src/app/components/AllDecks.tsx
+++ stbdy/src/app/components/AllDecks.tsx
@@ -10,7 +10,7 @@ async function getDecks() {
 export default async function AllDecks() {
   const decks = await getDecks();
   return (
-    <div className="mx-auto space-y-5 p-4 md:space-y-6 lg:space-y-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+    <div className="mx-auto space-y-4 p-4 md:space-y-6 lg:space-y-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 ">
       {decks.map((deck) => (
         <Deck key={deck.id} title={deck.title} description={deck.description} />
       ))}
--- stbdy/src/app/components/Deck.tsx
+++ stbdy/src/app/components/Deck.tsx
@@ -5,7 +5,7 @@ interface DeckProps {
 
 export default function Deck({ title, description }: DeckProps) {
   return (
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden m-6 max-w-md cursor-pointer hover:scale-105 transition-transform duration-100 ease-in-out">
+    <div className="bg-white shadow-lg rounded-lg overflow-hidden m-auto max-w-md cursor-pointer hover:scale-105 transition-transform duration-100 ease-in-out">
       <div className="p-8">
         <h2 className="text-2xl font-bold text-gray-900">{title}</h2>
         <p className="text-gray-600">{description}</p>
